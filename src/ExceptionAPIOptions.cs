de6739a84718cbe5729ec74321dcdeeb6c93833f
Kenneth Swan <49496839+kenswan@users.noreply.github.com>
2023-05-26 09:14:04 -0500

    #25 Add external API XML comments for development documentation

--- src/ExceptionAPIOptions.cs
+++ src/ExceptionAPIOptions.cs
@@ -8,13 +8,28 @@ using System.Net;
 
 namespace ExceptionsAPI;
 
+/// <summary>
+/// Configuration settings used to override default behavior of Exception API Middleware
+/// </summary>
 public class ExceptionAPIOptions
 {
+    /// <summary>
+    /// Default correlation key to use for the correlation id header
+    /// </summary>
     public string CorrelationKey { get; set; } = "X-Correlation-Id";
 
+    /// <summary>
+    /// Configure the correlation value during runtime of each request
+    /// </summary>
     public Func<HttpContext, string> ConfigureCorrelationValue { get; set; }
 
+    /// <summary>
+    /// Default error message if none specified for given type of exception
+    /// </summary>
     public string DefaultErrorMessage { get; set; } = "An internal error has occurred";
 
+    /// <summary>
+    /// Default error status code if none specified for given type of exception
+    /// </summary>
     public HttpStatusCode DefaultErrorStatusCode { get; set; } = HttpStatusCode.InternalServerError;
 }
--- src/IExceptionsAPIBuilder.cs
+++ src/IExceptionsAPIBuilder.cs
@@ -10,12 +10,31 @@ namespace ExceptionsAPI;
 
 public interface IExceptionsAPIBuilder
 {
+    /// <summary>
+    /// Configures a response status code for a given exception
+    /// </summary>
+    /// <typeparam name="TException">Type of exception that has being thrown</typeparam>
+    /// <param name="httpStatusCode">Configures status code returned for give exception</param>
+    /// <returns>Current Exception API Configuration Builder</returns>
     IExceptionsAPIBuilder AddException<TException>(HttpStatusCode httpStatusCode)
         where TException : Exception;
 
+    /// <summary>
+    /// Configures a response status code and default response message returned for a given exception
+    /// </summary>
+    /// <typeparam name="TException"></typeparam>
+    /// <param name="httpStatusCode"></param>
+    /// <param name="clientErrorMessage"></param>
+    /// <returns>Current Exception API Configuration Builder</returns>
     IExceptionsAPIBuilder AddException<TException>(HttpStatusCode httpStatusCode, string clientErrorMessage)
         where TException : Exception;
 
+    /// <summary>
+    /// Establish a response status code and response message (<see cref="ExceptionsAPIResponse"/>) returned for a given exception
+    /// </summary>
+    /// <typeparam name="TException"></typeparam>
+    /// <param name="buildExceptionResponse"></param>
+    /// <returns>Current Exception API Configuration Builder</returns>
     IExceptionsAPIBuilder AddException<TException>(Func<HttpContext, TException, ExceptionsAPIResponse> buildExceptionResponse)
         where TException : Exception;
 }
